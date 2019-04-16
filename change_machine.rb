d485d8b7a37bc852f417ef8fbdfa365ee0b2172b
Brian <riceb53@gmail.com>
2019-04-15 20:09:57 -0500

    pass first test

--- change_machine.rb
+++ change_machine.rb
@@ -1,11 +1,17 @@
 require 'rspec'
 
+class ChangeMachine
+  def change(cents)
+    return [1]
+  end
+end
+
 RSpec.describe ChangeMachine do
   describe '#change' do
     it 'should return [1] if given 1' do
       change_machine = ChangeMachine.new
       result = change_machine.change(1)
-      expect(result).to eq([1])
+      expect(change_machine.change(1)).to eq([1])
     end
   end
 end
